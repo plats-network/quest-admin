25de2e332a2cfd1fd5dd5fdcef5d39e1d844dd03
tranchinh <chinhlong30032001@gmail.com>
2023-11-28 10:17:21 +0700

    fix hashtag

--- src/components/Twitter/HashTag.jsx
+++ src/components/Twitter/HashTag.jsx
@@ -4,7 +4,13 @@ import BlockTask from "../BlockTask";
 function HashTag({ setHashtag, setActionTwitter, value, isDisable, urlHashtag, setUrlHashtag }) {
   return (
     <BlockTask title="Hashtag" setActionTwitter={setActionTwitter}>
-      <BlockInput name="Hashtags (separated by comma)" value={value} setAction={setHashtag} isDisable={isDisable} />
+      <BlockInput
+        name="Hashtags (separated by comma)"
+        value={value}
+        setAction={setHashtag}
+        isDisable={isDisable}
+        placeholder="#hashtag"
+      />
       <BlockInput name="URL" value={urlHashtag} setAction={setUrlHashtag} isDisable={isDisable} />
     </BlockTask>
   );
