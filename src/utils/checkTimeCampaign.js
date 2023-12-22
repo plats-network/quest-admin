2d1e7d6a21589b824f10084acfd21a5949f05afd
tranchinh <chinhlong30032001@gmail.com>
2023-12-22 11:00:07 +0700

    fix time

--- src/utils/checkTimeCampaign.js
+++ src/utils/checkTimeCampaign.js
@@ -4,7 +4,7 @@ const DAY = 1000 * 60 * 60 * 24;
 export const checkTimeCampaign = (startDate, endDate) => {
   const start_date = new Date(startDate).getTime();
   const end_date = new Date(endDate).getTime();
-  if (end_date - start_date < DAY) {
+  if (end_date - start_date < 0) {
     notifyError("Campaign time is invalid. please check again");
     return false;
   }
